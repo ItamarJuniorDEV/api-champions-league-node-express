93d6af66179ab7c5815903cd957e0eea11699caf
Itamar Junior <itamar.junior@irriga.net>
2025-12-14 19:53:28 -0300

    feat: configurar express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("ola");
+import express, {json, Request, Response} from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ player: "Beckham" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at port http://localhost:${port}`)
+});
