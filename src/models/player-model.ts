6c9537c45b08d800f54465b196e52f24b3b5d598
Itamar Junior <itamar.junior@irriga.net>
2025-12-15 13:23:42 -0300

    feat: implementar listagem de todos usuários

--- src/models/player-model.ts
+++ src/models/player-model.ts
@@ -1,5 +1,17 @@
 export interface PlayerModel {
   id: number;
   name: string;
+  club: string;
+  nationality: string;
+  position: string;
+  statistics: {
+    Overall: Number;
+    Pace: Number;
+    Shooting: Number;
+    Passing: Number;
+    Dribbling: Number;
+    Defending: Number;
+    Physical: Number;
+  }
 }
 
--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -13,3 +13,4 @@ export const getPlayerService = async () => {
 
   return response;
 };
+
